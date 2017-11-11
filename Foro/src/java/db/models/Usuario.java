ea896ad6d27686c000bb8f8e027358198a8ada61
ruizcauich <augusto.ruizcauich@gmail.com>
2017-11-11 18:06:44 -0500

    Implementación de getObjects en la clase Usuario, así como modificación de uno se sus paráetros del método desde la clase db.Models

--- Foro/src/java/db/models/Usuario.java
+++ Foro/src/java/db/models/Usuario.java
@@ -18,10 +18,26 @@ import html.HtmlModel;
 public class Usuario extends Model implements HtmlModel {
     
     private String nombre;
-    
-    public Usuario( int id, String nombre){
+    private String apellidos;
+    private String institucion;
+    private String nickname;
+    private String contrasena;
+    private String email;
+    private String avatar;
+    private boolean esModerador; 
+    public Usuario( int id, String nombre, String apellidos, String
+            institucion, String nickname, String contrasena, String
+                    email, String avatar, boolean esModerador){
+        
         this.id = id;
         this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.institucion = institucion;
+        this.nickname = nickname;
+        this.contrasena = contrasena;
+        this.email= email;
+        this.avatar = avatar;
+        this.esModerador = esModerador;
     }
     
     public Usuario(){};
@@ -32,8 +48,42 @@ public class Usuario extends Model implements HtmlModel {
     }
 
     @Override
-    public ArrayList<Model> getObjects(String[] params) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public ArrayList<Model> getObjects(String[][] atributo_valor) {
+        ArrayList<Model> usrs = new ArrayList<Model>();
+        String query = "SELECT * FROM Usuarios WHERE ";
+        for( int atributo = 0; atributo< atributo_valor.length; atributo++){
+            query += atributo_valor[ atributo ][0] + "='";
+            query += atributo_valor[ atributo ][1] + "' ";
+            if( atributo== (atributo_valor.length-1)){
+                query += ";";
+            }else{
+                query += " OR ";
+            }
+        }
+        ResultSet usuarios = this.connection.ejecutarConsulta(query);
+        try {
+            // Recupera datos de cada fila
+            while( usuarios.next()){
+                this.id = usuarios.getInt("id");
+                this.nombre = usuarios.getString("nombre");
+                this.apellidos = usuarios.getString("apellidos");
+                this.institucion = usuarios.getString("institucion");
+                this.nickname = usuarios.getString("nickname");
+                this.contrasena = usuarios.getString("contrasena");
+                this.email = usuarios.getString("email");
+                this.avatar = usuarios.getString("avatar");
+                this.esModerador = usuarios.getBoolean("es_moderador");
+                usrs.add( new Usuario(id, nombre, apellidos, institucion,
+                nickname, contrasena, email, avatar, esModerador)
+                );
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Usuario.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return usrs;
+        
+        
     }
 
     @Override
@@ -44,9 +94,18 @@ public class Usuario extends Model implements HtmlModel {
         try {
             // Recupera datos de cada fila
             while( usuarios.next()){
-                int id = usuarios.getInt("id");
-                String nombre = usuarios.getString("nombre");
-                usrs.add( new Usuario(id, nombre));
+                this.id = usuarios.getInt("id");
+                this.nombre = usuarios.getString("nombre");
+                this.apellidos = usuarios.getString("apellidos");
+                this.institucion = usuarios.getString("institucion");
+                this.nickname = usuarios.getString("nickname");
+                this.contrasena = usuarios.getString("contrasena");
+                this.email = usuarios.getString("email");
+                this.avatar = usuarios.getString("avatar");
+                this.esModerador = usuarios.getBoolean("es_moderador");
+                usrs.add( new Usuario(id, nombre, apellidos, institucion,
+                nickname, contrasena, email, avatar, esModerador)
+                );
             }
         } catch (SQLException ex) {
             Logger.getLogger(Usuario.class.getName()).log(Level.SEVERE, null, ex);
@@ -59,5 +118,5 @@ public class Usuario extends Model implements HtmlModel {
     public String toHtml() {
         return "<h1>" + nombre+ "</h1>";
     }
-    
+    public String toString(){ return nombre;} 
 }
